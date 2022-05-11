ed2984abaf94ad0177aa8e1d52df7cabdf661c55
Clay Xiong <klayxi@gmail.com>
2022-05-11 18:42:30 +0900

    added styling to buttons

--- src/Components/ContactButtons.js
+++ src/Components/ContactButtons.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 export default function ContactButtons() {
+
+    // reusable button component
+    function ContactButton(props) {
+        return (
+            <button className={`btn ${props.addClass}`}>{props.text}</button>
+        )
+    }
+
     return (
         <div className="contact-buttons">
-            
+            <ContactButton text="Email" addClass="email" />
+            <ContactButton text="LinkedIn" addClass="linkedin"/>
         </div>
     );
   }
