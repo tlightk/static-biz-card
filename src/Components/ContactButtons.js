514953ee862917fac07eb943dc802e39b2abf0ca
Clay Xiong <klayxi@gmail.com>
2022-05-12 18:29:24 +0900

    fixed icons for buttons

--- src/Components/ContactButtons.js
+++ src/Components/ContactButtons.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function ContactButtons() {
 
     // reusable button component
     function ContactButton(props) {
         return (
-            <button className={`btn ${props.addClass}`}>{props.text}</button>
+            <div className="btn-wrapper">
+                <FontAwesomeIcon icon={brands("twitter-square")} className={`contact-icon ${props.addIconClass}`} />
+                <button className={`btn ${props.addBtnClass}`}>{props.text}</button>
+            </div>
+            
         )
     }
 
     return (
         <div className="contact-buttons">
-            <ContactButton text="Email" addClass="email" />
-            <ContactButton text="LinkedIn" addClass="linkedin"/>
+            <ContactButton text="Email" addBtnClass="email" addIconClass="email-icon" />
+            <ContactButton text="LinkedIn" addBtnClass="linkedin" addIconClass="linkedin-icon" />
         </div>
     );
   }
