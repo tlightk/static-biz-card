5c88c86ffc9915de8fceef62e373e0939313b2ab
Clay Xiong <klayxi@gmail.com>
2022-05-12 17:58:22 +0900

    added fontawesome and icons for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Footer() {
-    return (
-        <div className="footer">
-            
-        </div>
-    );
-  }
+  return (
+    <div className="footer">
+      <FontAwesomeIcon icon={brands("twitter-square")} />
+      <FontAwesomeIcon icon={brands("facebook-square")} />
+      <FontAwesomeIcon icon={brands("instagram-square")} />
+      <FontAwesomeIcon icon={brands("github-square")} />
+    </div>
+  );
+}
